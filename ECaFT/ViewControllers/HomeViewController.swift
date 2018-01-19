300eb20be69a8b1df66b6166150ab9af9e0602c6
Amanda Ong <ayo3@cornell.edu>
2018-01-18 21:19:44 -0500

    Made homepage

--- ECaFT/ViewControllers/HomeViewController.swift
+++ ECaFT/ViewControllers/HomeViewController.swift
@@ -10,26 +10,17 @@ import UIKit
 
 class HomeViewController: UIViewController {
     let screenSize : CGRect = UIScreen.main.bounds
-    var careerFairTitle = UILabel()
-    var bodyParagraph = UITextView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         hideFilterBtn()
         
         view.backgroundColor = UIColor.black
-        
-        let navBarHeight = tabBarItem.accessibilityFrame.height
-        let backgroundHeight = view.frame.height - navBarHeight
-        let backgroundImageView = UIImageView(frame: CGRect(origin: CGPoint(x: 0, y: navBarHeight), size: CGSize(width: view.frame.width, height: backgroundHeight)))
-
-        backgroundImageView.image = #imageLiteral(resourceName: "ecaftBackground")
-        makeTitle()
-        makeBodyParagraph()
-        
-        view.addSubview(backgroundImageView)
-        view.sendSubview(toBack: backgroundImageView)
-        
+    
+        makeFairTitle()
+        makeLocationTitle()
+        makeBanner()
+        makeBackground()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -43,43 +34,47 @@ class HomeViewController: UIViewController {
         self.navigationController?.navigationBar.topItem?.rightBarButtonItem = nil
     }
     
-    private func makeTitle() {
-        careerFairTitle = UILabel(frame: CGRect(x: 0, y: 0, width: screenSize.width*0.8, height: 30))
-        careerFairTitle.center.x = 0.5*screenSize.width
+    private func makeLocationTitle() {
+        let locationTitle = UILabel(frame: CGRect(x: 0, y: 180, width: screenSize.width*0.72, height: 30))
+        locationTitle.center.x = 0.5*screenSize.width
         if(screenSize.height < 667.0) { //For iPhone 5s & below
-            careerFairTitle.center.y = 70
-        } else {
-            careerFairTitle.center.y = 130
+            locationTitle.center.y = 135
         }
-        careerFairTitle.textAlignment = NSTextAlignment.center
-        careerFairTitle.font = UIFont.boldSystemFont(ofSize: 20)
-        careerFairTitle.textColor = UIColor.white
-        careerFairTitle.text = "Welcome to the \n2017 Cornell Spring Technical & Entreprenurial Career Fair!"
+        locationTitle.textAlignment = NSTextAlignment.center
+        locationTitle.font = UIFont(name: "Avenir-Medium", size: 28.0)
+        locationTitle.textColor = UIColor.white
+        locationTitle.text = "Wednesday, Feb. 8th \n9:00 am - 2:00 pm \nBarton Hall"
         
         //Make title go onto other lines
-        careerFairTitle.numberOfLines = 0 //set num of lines to infinity
-        careerFairTitle.lineBreakMode = .byWordWrapping
-        careerFairTitle.sizeToFit()
-        view.addSubview(careerFairTitle)
+        locationTitle.numberOfLines = 0 //set num of lines to infinity
+        locationTitle.lineBreakMode = .byWordWrapping
+        locationTitle.sizeToFit()
+        view.addSubview(locationTitle)
     }
     
-    private func makeBodyParagraph() {
-        bodyParagraph = UITextView(frame: CGRect(x: 0, y: 0, width: screenSize.width*0.8, height: 400))
-        bodyParagraph.center.x = 0.5*screenSize.width
-        if(screenSize.height < 667.0) { //For iPhone 5s & below
-            bodyParagraph.center.y = 360
-        } else {
-            bodyParagraph.center.y = 400
-        }
-        
-        bodyParagraph.textAlignment = NSTextAlignment.center
-        bodyParagraph.font = UIFont.systemFont(ofSize: 15)
-        bodyParagraph.textColor = UIColor.white
-        bodyParagraph.backgroundColor = UIColor.clear
-        bodyParagraph.isScrollEnabled = false
-        bodyParagraph.isSelectable = false
-        bodyParagraph.text = "The fair is at Barton Hall on Wednesday, February 8th and runs from 9:00am to 2:00pm. We recommend dressing up in formal attire and having multiple copies of your resume and a notepad to keep track of recruiter info. Remember to do your research before-hand and learn more about each company's open positions to show that you're prepared. Good luck!"
-        view.addSubview(bodyParagraph)
+    private func makeFairTitle() {
+        let fairBanner = UIImageView(frame: CGRect(x: 0, y: 15, width: screenSize.width*0.65, height: 75))
+        fairBanner.image = #imageLiteral(resourceName: "FairBanner")
+        fairBanner.contentMode = UIViewContentMode.scaleAspectFit
+        view.addSubview(fairBanner)
+    }
+    
+    private func makeBanner() {
+        let banner = UIImageView(frame: CGRect(x: 0, y: 0, width: screenSize.width, height: 125))
+        banner.image = #imageLiteral(resourceName: "HomeBanner")
+        banner.contentMode = UIViewContentMode.scaleAspectFit
+        banner.center.y = screenSize.height*0.7
+        banner.center.x = screenSize.width - (banner.frame.width/2.0) + 15
+        view.addSubview(banner)
+    }
+    
+    private func makeBackground() {
+        let navBarHeight = tabBarItem.accessibilityFrame.height
+        let backgroundHeight = view.frame.height - navBarHeight
+        let backgroundImageView = UIImageView(frame: CGRect(origin: CGPoint(x: 0, y: navBarHeight), size: CGSize(width: view.frame.width, height: backgroundHeight)))
+        backgroundImageView.image = #imageLiteral(resourceName: "ecaftBackground")
+        view.addSubview(backgroundImageView)
+        view.sendSubview(toBack: backgroundImageView)
     }
     
 }
